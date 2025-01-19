2a59136c12e6bc8b26f7db465a6b447c2c5224e9
ruugii_31 <roger.barrero.sorribas@gmail.com>
2025-01-19 12:17:03 +0100

    Home page

---
+++ src/app/components/Home/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <section className="flex flex-col gap-8 row-start-2 items-center sm:items-start bg-white rounded-lg p-8 text-black" id="home">
+      <h1 className="text-4xl font-bold">
+        Welcome to my portfolio
+      </h1>
+      <p className="text-2xl">
+        My name is Roger, I&apos;m a curious fullstack engineer who loves to learn new things and build cool stuff.
+      </p>
+    </section>
+  );
+}
