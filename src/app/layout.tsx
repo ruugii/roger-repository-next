a54ef6d38e12459489261b09e51871402ed1da2b
ruugii_31 <roger.barrero.sorribas@gmail.com>
2025-01-19 12:03:44 +0100

    Create footer layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,10 +27,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} min-h-screen grid grid-rows-layout`}
       >
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
