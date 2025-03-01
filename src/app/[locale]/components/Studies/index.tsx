198ab2d21bf0eafc9f3a3c7aeb6226ce49f5b027
ruugii_31 <roger.barrero.sorribas@gmail.com>
2025-03-01 19:54:52 +0100

    fix show studies

--- src/app/[locale]/components/Studies/index.tsx
+++ src/app/[locale]/components/Studies/index.tsx
@@ -3,191 +3,190 @@ import { useTranslations } from "next-intl";
 
 export default function Studies() {
 
-  const t = useTranslations("studies");
+  const t = useTranslations('studies');
 
   return (
     <section className="flex flex-col gap-8 row-start-2 items-center sm:items-start bg-white rounded-lg p-8 text-black w-full" id="studies">
       <h2 className="text-4xl font-bold">
-        {t("title")}
+        {t('title')}
       </h2>
       <Timeline>
-        <TimelineItem className=" hidden">
+        <TimelineItem sx={{ display: "none" }}>
           <TimelineOppositeContent></TimelineOppositeContent>
           <TimelineSeparator>
             <TimelineConnector />
           </TimelineSeparator>
           <TimelineContent></TimelineContent>
         </TimelineItem>
-        <TimelineItem>
+        <TimelineItem sx={{ minHeight: "80px" }}>
           <TimelineOppositeContent>
             <b className="font-bold">
-              {`${t("year.2024")} - ${t("year.current")}`}
+              {t('year.2024')} - {t('year.current')}
             </b>
           </TimelineOppositeContent>
           <TimelineSeparator>
-            <TimelineDot />
+            <TimelineDot sx={{ width: "12px", height: "12px" }} />
             <TimelineConnector />
           </TimelineSeparator>
           <TimelineContent>
             <b className="font-bold">
-              {t("school.lasalleonline")}
+              {t('school.lasalleonline')}
             </b>
             <ul>
               <li>
-                {t("degree.DAM")}
+                {t('degree.DAM')}
               </li>
             </ul>
           </TimelineContent>
         </TimelineItem>
-        <TimelineItem>
+        <TimelineItem sx={{ minHeight: "80px" }}>
           <TimelineOppositeContent>
             <b className="font-bold">
-              {`${t("year.2024")} - ${t("year.2021")}`}
+              {t('year.2021')} - {t('year.2024')}
             </b>
           </TimelineOppositeContent>
           <TimelineSeparator>
-            <TimelineDot />
+            <TimelineDot sx={{ width: "12px", height: "12px" }} />
             <TimelineConnector />
           </TimelineSeparator>
           <TimelineContent>
             <b className="font-bold">
-              {t("school.lasallegracia")}
+              {t('school.lasallegracia')}
             </b>
             <ul>
               <li>
-                {t("degree.DAW")}
+                {t('degree.DAW')}
               </li>
             </ul>
           </TimelineContent>
         </TimelineItem>
-        <TimelineItem>
+        <TimelineItem sx={{ minHeight: "80px" }}>
           <TimelineOppositeContent>
             <b className="font-bold">
-              {`${t("year.2021")} - ${t("year.2018")}`}
+              {t('year.2018')} - {t('year.2021')}
             </b>
           </TimelineOppositeContent>
           <TimelineSeparator>
-            <TimelineDot />
+            <TimelineDot sx={{ width: "12px", height: "12px" }} />
             <TimelineConnector />
           </TimelineSeparator>
           <TimelineContent>
             <b className="font-bold">
-              {t("school.eugeni")}
+              {t('school.eugeni')}
             </b>
             <ul>
               <li>
-                {t("degree.SMX")}
+                {t('degree.SMX')}
               </li>
             </ul>
           </TimelineContent>
         </TimelineItem>
-        <TimelineItem>
+        <TimelineItem sx={{ minHeight: "80px" }}>
           <TimelineOppositeContent>
             <b className="font-bold">
-              {t("year.2023")}
+              {t('year.2023')}
             </b>
           </TimelineOppositeContent>
           <TimelineSeparator>
-            <TimelineDot />
+            <TimelineDot sx={{ width: "12px", height: "12px" }} />
             <TimelineConnector />
           </TimelineSeparator>
           <TimelineContent>
             <b className="font-bold">
-              {t("school.lucatic")}
+              {t('school.lucatic')}
             </b>
             <ul>
               <li>
-                {t("degree.InTalent")}
+                {t('degree.InTalent')}
               </li>
             </ul>
           </TimelineContent>
         </TimelineItem>
-        <TimelineItem>
+        <TimelineItem sx={{ minHeight: "80px" }}>
           <TimelineOppositeContent>
             <b className="font-bold">
-              {t("year.2022")}
+              {t('year.2022')}
             </b>
           </TimelineOppositeContent>
           <TimelineSeparator>
-            <TimelineDot />
+            <TimelineDot sx={{ width: "12px", height: "12px" }} />
             <TimelineConnector />
           </TimelineSeparator>
           <TimelineContent>
             <b className="font-bold">
-              {t("school.udemy")}
+              {t('school.udemy')}
             </b>
             <ul>
               <li>
-                {t("degree.Solidity")}
+                {t('degree.Solidity')}
               </li>
             </ul>
           </TimelineContent>
         </TimelineItem>
-        <TimelineItem>
+        <TimelineItem sx={{ minHeight: "80px" }}>
           <TimelineOppositeContent>
             <b className="font-bold">
-              {t("year.2021")}
+              {t('year.2021')}
             </b>
           </TimelineOppositeContent>
           <TimelineSeparator>
-            <TimelineDot />
+            <TimelineDot sx={{ width: "12px", height: "12px" }} />
             <TimelineConnector />
           </TimelineSeparator>
           <TimelineContent>
             <b className="font-bold">
-              {t("school.CET")}
+              {t('school.CET')}
             </b>
             <ul>
               <li>
-                {t("degree.Diamizer")}
+                {t('degree.Diamizer')}
               </li>
             </ul>
           </TimelineContent>
         </TimelineItem>
-        <TimelineItem>
+        <TimelineItem sx={{ minHeight: "80px" }}>
           <TimelineOppositeContent>
             <b className="font-bold">
-              {t("year.2020")}
+              {t('year.2020')}
             </b>
           </TimelineOppositeContent>
           <TimelineSeparator>
-            <TimelineDot />
+            <TimelineDot sx={{ width: "12px", height: "12px" }} />
             <TimelineConnector />
           </TimelineSeparator>
           <TimelineContent>
             <b className="font-bold">
-              {t("school.CET")}
+              {t('school.CET')}
             </b>
             <ul>
               <li>
-                {t("degree.monitor")}
+                {t('degree.monitor')}
               </li>
             </ul>
           </TimelineContent>
         </TimelineItem>
-        <TimelineItem>
+        <TimelineItem sx={{ minHeight: "80px" }}>
           <TimelineOppositeContent>
             <b className="font-bold">
-              {t("year.2018")}
+              {t('year.2018')}
             </b>
           </TimelineOppositeContent>
           <TimelineSeparator>
-            <TimelineDot />
+            <TimelineDot sx={{ width: "12px", height: "12px" }} />
             <TimelineConnector />
           </TimelineSeparator>
           <TimelineContent>
             <b className="font-bold">
-              {t("school.CISCO")}
+              {t('school.CISCO')}
             </b>
             <ul>
               <li>
-                {t("degree.CCNA")}
+                {t('degree.CCNA')}
               </li>
             </ul>
           </TimelineContent>
         </TimelineItem>
       </Timeline>
     </section>
-
   )
 }
