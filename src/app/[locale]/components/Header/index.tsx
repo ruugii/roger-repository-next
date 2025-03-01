3c7a73f23d499629bdb11ba4b278ba5f2b42358a
ruugii_31 <roger.barrero.sorribas@gmail.com>
2025-03-01 19:57:11 +0100

    show only pc

--- src/app/[locale]/components/Header/index.tsx
+++ src/app/[locale]/components/Header/index.tsx
@@ -4,7 +4,7 @@ import OptionsHeader from "./OptinsHeader";
 export default function Header() {
 
   return (
-    <header className=" fixed top-0 left-0 w-full bg-slate-50 text-black z-10 flex justify-between items-center py-4 px-20 gap-16">
+    <header className=" fixed top-0 left-0 w-full bg-slate-50 text-black z-10 justify-between items-center py-4 px-20 gap-16 hidden lg:flex">
       <OptionsHeader />
       <LangHeader />
     </header>
