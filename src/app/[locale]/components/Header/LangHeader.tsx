cd0d26ef2ca19affdb64cc383da11038d3890bb4
ruugii_31 <roger.barrero.sorribas@gmail.com>
2025-03-13 09:06:59 +0100

    Mantener el open al cambiar el idioma

--- src/app/[locale]/components/Header/LangHeader.tsx
+++ src/app/[locale]/components/Header/LangHeader.tsx
@@ -1,23 +1,26 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname, useSearchParams } from "next/navigation";
 
 export default function LangHeader() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const queryString = searchParams.toString(); // Convierte los parámetros en string
+  const hash = typeof window !== "undefined" ? window.location.hash : ""; // Obtiene el hash de la URL
+
   return (
     <ul className="flex gap-4">
-      <li className=" text-black hover:text-gray-800 hover:underline">
-        <Link
-          href="/es"
-          locale="es"
-        >
+      <li className="text-black hover:text-gray-800 hover:underline">
+        <Link href={`/es${queryString ? `?${queryString}` : ""}${hash}`} locale="es">
           ES
         </Link>
       </li>
-      <li className=" text-black hover:text-gray-800 hover:underline">
-        <Link
-          href="/en"
-          locale="en"
-        >
-          EN</Link>
+      <li className="text-black hover:text-gray-800 hover:underline">
+        <Link href={`/en${queryString ? `?${queryString}` : ""}${hash}`} locale="en">
+          EN
+        </Link>
       </li>
     </ul>
-  )
+  );
 }
