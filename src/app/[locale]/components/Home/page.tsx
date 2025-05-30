0d358d0d10cf5c44c9f2b696810db31b1e7e2dcc
ruugii_31 <roger.barrero.sorribas@gmail.com>
2025-05-30 13:03:36 +0200

    fix: add width to Home component section for better layout consistency

--- src/app/[locale]/components/Home/page.tsx
+++ src/app/[locale]/components/Home/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   const t = useTranslations('home')
 
   return (
-    <section className="flex flex-col gap-8 row-start-2 items-center sm:items-start bg-white rounded-lg p-8 text-black" id="home">
+    <section className="flex flex-col gap-8 row-start-2 items-center sm:items-start bg-white rounded-lg p-8 text-black w-full" id="home">
       <h1 className="text-4xl font-bold">
         {t('welcome')}
       </h1>
