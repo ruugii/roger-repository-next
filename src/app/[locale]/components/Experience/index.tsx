89a4a3a15b46e47bd3c6a4b356a315b57791c4a1
ruugii_31 <roger.barrero.sorribas@gmail.com>
2025-09-01 19:32:53 +0200

    add leti to experience

--- src/app/[locale]/components/Experience/index.tsx
+++ src/app/[locale]/components/Experience/index.tsx
@@ -14,6 +14,22 @@ export default function Experience() {
       <Timeline
         items={[
           {
+            date: `${t('year.2025')}`,
+            title: t('enterprice.leti.title'),
+            description: t.rich('enterprice.leti.description', {
+              ul: (chunks) => (
+                <ul>
+                  {chunks}
+                </ul>
+              ),
+              li: (chunks) => (
+                <li>
+                  {chunks}
+                </li>
+              )
+            }),
+          },
+          {
             date: `${t('year.2025')} - ${t('year.current')}`,
             title: t('enterprice.cem.title'),
             description: t.rich('enterprice.cem.description', {
