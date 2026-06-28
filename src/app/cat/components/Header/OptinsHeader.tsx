ae71a6d703c031d381f4085c98db2da92efbace0
ruugii_31 <90140860+ruugii@users.noreply.github.com>
2026-06-28 11:48:28 +0200

    cat header update

--- src/app/cat/components/Header/OptinsHeader.tsx
+++ src/app/cat/components/Header/OptinsHeader.tsx
@@ -8,22 +8,22 @@ export default function OptionsHeader() {
   return (
     <ul className="site-nav">
       <li>
-        <Link href="/#home">{t('home')}</Link>
+        <Link href="/cat#home">{t('home')}</Link>
       </li>
       <li>
-        <Link href="/#about">{t('about')}</Link>
+        <Link href="/cat#about">{t('about')}</Link>
       </li>
       <li>
-        <Link href="/#projects">{t('projects')}</Link>
+        <Link href="/cat#projects">{t('projects')}</Link>
       </li>
       <li>
-        <Link href="/#skills">{t('skills')}</Link>
+        <Link href="/cat#skills">{t('skills')}</Link>
       </li>
       <li>
-        <Link href="/#studies">{t('studies')}</Link>
+        <Link href="/cat#studies">{t('studies')}</Link>
       </li>
       <li>
-        <Link href="/#experience">{t('experience')}</Link>
+        <Link href="/cat#experience">{t('experience')}</Link>
       </li>
     </ul>
   )
